5b94ab5fc345cadb491990a3aaaf0e88488e3fb3
ltr <apuroopgupca007@gmail.com>
2024-05-12 17:49:18 +0530

    Added logic for Ratings component

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,9 +1,61 @@
-import React from 'react'
-import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
-}
+import React from "react";
+import styled from "styled-components";
+import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
+const Stars = ({ rating, reviews }) => {
+  const ratings = [];
+  if (rating % 5 === 0) {
+    for (let i = 0; i < 5; i++) {
+      ratings.push(
+        parseInt(rating) === 0 ? (
+          <span key={i + "empty"}>
+            <BsStar />
+          </span>
+        ) : (
+          <span key={i + "full"}>
+            <BsStarFill />
+          </span>
+        )
+      );
+    }
+  } else {
+    for (let i = 0; i < Math.floor(rating); i++) {
+      ratings.push(
+        <span key={i}>
+          <BsStarFill />
+        </span>
+      );
+    }
+    if (rating - Math.floor(rating) >= 0.5) {
+      ratings.push(
+        <span key={rating - Math.floor(rating)}>
+          <BsStarHalf />
+        </span>
+      );
+    } else {
+      ratings.push(
+        <span key={rating - Math.floor(rating)}>
+          <BsStar />
+        </span>
+      );
+    }
+    for (let i = Math.ceil(rating); i < 5; i++) {
+      ratings.push(
+        <span key={i}>
+          <BsStar />
+        </span>
+      );
+    }
+  }
+
+  return (
+    <Wrapper>
+      <div className="stars">
+        <span>{ratings}</span>
+      </div>
+      <p className="reviews">({reviews} customer reviews)</p>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   display: flex;
@@ -18,5 +70,5 @@ const Wrapper = styled.div`
     margin-bottom: 0;
   }
   margin-bottom: 0.5rem;
-`
-export default Stars
+`;
+export default Stars;
