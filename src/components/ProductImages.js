0657d156e662768719b0586703484905ac351ef4
ltr <apuroopgupca007@gmail.com>
2024-05-12 14:12:30 +0530

    Setup Product images section in PDP

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,9 +1,31 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
-const ProductImages = () => {
-  return <h4>product images</h4>
-}
+const ProductImages = ({ images }) => {
+  const [activeImgIdx, setActiveImgIdx] = useState(0);
+  const galleryClickHandler = (imgIdx) => {
+    setActiveImgIdx(imgIdx);
+  };
+  return (
+    <Wrapper className="main">
+      <img
+        src={images[activeImgIdx].url}
+        alt={images[activeImgIdx]?.filename}
+      />
+      <div className="gallery">
+        {images.map((img, idx) => (
+          <img
+            onClick={() => galleryClickHandler(idx)}
+            className={idx === activeImgIdx ? "active" : ""}
+            key={img.id}
+            src={img.url}
+            alt={img.filename}
+          />
+        ))}
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   .main {
@@ -48,6 +70,6 @@ const Wrapper = styled.section`
       }
     }
   }
-`
+`;
 
-export default ProductImages
+export default ProductImages;
--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -34,9 +34,55 @@ const SingleProductPage = () => {
       }, 3000);
     }
   }, [error]);
+
+  if (loading) {
+    return <Loading />;
+  } else if (error) {
+    return <Error />;
+  }
+
+  const {
+    name,
+    price,
+    description,
+    stock,
+    stars,
+    reviews,
+    id: sku,
+    company,
+    images,
+  } = product;
   return (
     <Wrapper>
-      {loading ? <Loading /> : error ? <Error /> : <h1>Single Product</h1>}
+      <PageHero title={name} product />
+      <div className="section section-center page">
+        <Link to="/products" className="btn">
+          Back to Products
+        </Link>
+        <div className="product-center">
+          <section>{images && <ProductImages images={images} />}</section>
+          <section className="content">
+            <h2>{name}</h2>
+            <Stars />
+            <h5 className="price">{formatPrice(price)}</h5>
+            <p className="desc">{description}</p>
+            <p className="info">
+              <span>Available: </span>
+              {stock > 0 ? "In Stock" : "Out of Stock"}
+            </p>
+            <p className="info">
+              <span>SKU: </span>
+              {sku}
+            </p>
+            <p className="info">
+              <span>Brand: </span>
+              {company}
+            </p>
+            <hr />
+            {stock > 0 && <AddToCart />}
+          </section>
+        </div>
+      </div>
     </Wrapper>
   );
 };
