d3f4166230f932b0cee99b9d37e5799db36a18a7
ltr <apuroopgupca007@gmail.com>
2024-05-14 15:47:33 +0530

    Setup ProductList and GridView

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import { useFilterContext } from '../context/filter_context'
-import GridView from './GridView'
-import ListView from './ListView'
+import React from "react";
+import { useFilterContext } from "../context/filter_context";
+import GridView from "./GridView";
+import ListView from "./ListView";
 
 const ProductList = () => {
-  return <h4>product list</h4>
-}
-
-export default ProductList
+  const { filtered_products: products, grid_view } = useFilterContext();
+  if (products.length === 0) {
+    return <h5>Sorry, no Products matched your search...</h5>;
+  }
+  return !grid_view ? (
+    <ListView products={products}> </ListView>
+  ) : (
+    <GridView products={products}> </GridView>
+  );
+};
+export default ProductList;
